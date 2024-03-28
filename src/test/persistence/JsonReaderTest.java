8a55575173635dca7bacdb93a67c6ae52fbaf0ee
Jennifer <jennifer26wang@gmail.com>
2024-03-28 11:09:36 -0700

    Fix bug

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
-class JsonReaderTest {
+public class JsonReaderTest {
     private ExpenseManagerUI ui = new ExpenseManagerUI();
 
     @Test
--- src/test/persistence/JsonWriterTest.java
+++ src/test/persistence/JsonWriterTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
-class JsonWriterTest {
+public class JsonWriterTest {
     //NOTE TO CPSC 210 STUDENTS: the strategy in designing tests for the JsonWriter is to
     //write data to a file and then use the reader to read it back in and check that we
     //read in a copy of what was written out.
